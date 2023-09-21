0c863f0554d143afbec62392561afff82d4d5b97
Allan (Slam) <72238104+aayes89@users.noreply.github.com>
2023-09-21 13:45:12 -0600

    Update rpipicorgh.c

--- rpipicorgh.c
+++ rpipicorgh.c
@@ -13,6 +13,27 @@
 #define DEBUG_LED_PIN 25
 #define GPIO_TRIGGER_PIN 15
 
+
+void measure_freqs(void) {
+    uint f_pll_sys = frequency_count_khz(CLOCKS_FC0_SRC_VALUE_PLL_SYS_CLKSRC_PRIMARY);
+    uint f_pll_usb = frequency_count_khz(CLOCKS_FC0_SRC_VALUE_PLL_USB_CLKSRC_PRIMARY);
+    uint f_rosc = frequency_count_khz(CLOCKS_FC0_SRC_VALUE_ROSC_CLKSRC);
+    uint f_clk_sys = frequency_count_khz(CLOCKS_FC0_SRC_VALUE_CLK_SYS);
+    uint f_clk_peri = frequency_count_khz(CLOCKS_FC0_SRC_VALUE_CLK_PERI);
+    uint f_clk_usb = frequency_count_khz(CLOCKS_FC0_SRC_VALUE_CLK_USB);
+    uint f_clk_adc = frequency_count_khz(CLOCKS_FC0_SRC_VALUE_CLK_ADC);
+    uint f_clk_rtc = frequency_count_khz(CLOCKS_FC0_SRC_VALUE_CLK_RTC);
+ 
+    printf("pll_sys  = %dkHz\n", f_pll_sys);
+    printf("pll_usb  = %dkHz\n", f_pll_usb);
+    printf("rosc     = %dkHz\n", f_rosc);
+    printf("clk_sys  = %dkHz\n", f_clk_sys);
+    printf("clk_peri = %dkHz\n", f_clk_peri);
+    printf("clk_usb  = %dkHz\n", f_clk_usb);
+    printf("clk_adc  = %dkHz\n", f_clk_adc);
+    printf("clk_rtc  = %dkHz\n", f_clk_rtc);
+}
+
 // Función para configurar los pines GPIO en RP2040
 void configurePins() {
     gpio_init(CPU_RESET_PIN);
@@ -48,6 +69,9 @@ void performGlitch() {
 
 
 int main(void) {
+    // Medir la frecuencia
+    measure_freqs();
+    
     // Voltage min 0.85 (0b0110) y máx 1.30 (0b1111)
     vreg_set_voltage(VREG_VOLTAGE_1_30); 
     
@@ -57,7 +81,12 @@ int main(void) {
     // Obtengo frecuencia del reloj de sistema, puede incluir índice como parámetro
     uint32_t freq = clock_get_hz(clk_sys);
     
-    //CLOCKS_CLK_PERI_CTRL_AUXSRC_A.CLK_SYS 
+    // Liberar/des-inicializar el PLL especificado del RP2040
+    // pll_sys: hasta 180MHZ y pll_usb: hasta 48MHZ
+    // pll_deinit(pll_sys);
+    
+    // Trabajar con el PLL_USB, cambiar (freq) por la requerida * MHZ
+    // Comentar línea donde se obtiene la frequencia para modo manual
     /*
     pub enum AUXSRC_A {
         CLK_SYS,
@@ -69,10 +98,8 @@ int main(void) {
         CLKSRC_GPIN1,
     }
     */
-    // Vía simple para trabajar con el PLL_USB, cambiar (freq) por la requerida * MHZ
-    // Comentar línea donde se obtiene la frequencia para modo manual
     clock_configure(clk_peri, 0, CLOCKS_CLK_PERI_CTRL_AUXSRC_VALUE_CLK_SYS, freq, freq);
-
+    
     // Configurar los pines GPIO
     configurePins();
     
